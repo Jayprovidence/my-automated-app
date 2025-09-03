7edabacc8e0c9c83e6d6261c8ed565996d377f44
Jack <a0986254299@gmail.com>
2025-09-03 10:05:39 +0800

    Final fix: Set logging to cloud logging only

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('版本 3：自動化部署成功！CI/CD 管道正常運作！');
+  res.send('版本 4：自動化部署成功！CI/CD 管道正常運作！');
 });
 app.listen(port, () => {
   console.log(`應用程式正在監聽 port ${port}`);
